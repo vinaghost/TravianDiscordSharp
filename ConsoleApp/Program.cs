a9f7096ba1a5754d00a954da48744791cf201d40
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-04-10 23:35:19 +0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            var secretProvider = config.Providers.First();
+            secretProvider.TryGet("MongoDBConnectionString", out var connectionString);
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(connectionString);
+        }
+    }
+}
