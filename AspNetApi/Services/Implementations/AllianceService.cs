519bad51e2415f72dd95c6ea46768df8196caaa4
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-04-22 20:51:22 +0700

    add sort base on pop to ally & player village

--- AspNetApi/Services/Implementations/AllianceService.cs
+++ AspNetApi/Services/Implementations/AllianceService.cs
@@ -25,7 +25,7 @@ namespace AspNetApi.Services.Implementations
         {
             var collection = _mongoDbService.GetVillages(world);
             var filter = Builders<Village>.Filter.Where(x => x.AllyId == id);
-            return collection.Find(filter).ToEnumerable();
+            return collection.Find(filter).SortBy(x => x.Pop).ToEnumerable();
         }
     }
 }
